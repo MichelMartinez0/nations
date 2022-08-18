db999fd069f46e0b95362d9ab6d358b7d602d58c
camiloamaya <caamaya476@misena.edu.co>
2022-08-18 11:59:38 -0500

    relaciones

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -1,11 +1,65 @@
 <?php
 
+
+
+
 namespace App\Models;
 
+
+
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
+
+
+
 class Country extends Model
+
 {
+
+    protected $table = 'countries';
+
+    protected $primaryKey = "country_id";
+
     use HasFactory;
+
+
+
+
+    public function region(){
+
+        //BelongsTo parametros
+
+        //1)modelo a relacionar
+
+        //2)la llave foranea el el modelo actual
+
+
+
+
+        return $this->hasMany(Region::class,'country_id');
+
+    }
+
+
+
+
+    public function idiomas(){
+
+        //parametros
+
+        //1 modelo a relacionar
+
+        //2 tabla intermedia
+
+        //3 clave foranea de el modelo actual en la tabla intermedia
+
+        //4 clave foranea de el modelo a relacionar en la tabla intermedia
+
+        return $this->belongsToMany(Languaje::class,'country_languages','country_id','language_id');
+
+    }
+
 }
